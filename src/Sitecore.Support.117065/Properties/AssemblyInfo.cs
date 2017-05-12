6e07c7c5cd0bdbfcae6febafddd2a9a9be22a318
ido <ido@WS-IDO1UA1>
2017-05-12 22:11:02 +0300

    First commit - patch is ready

---
+++ src/Sitecore.Support.117065/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.117065")]
+[assembly: AssemblyProduct("Sitecore.Support.117065")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("List View in the Select Media dialog does not display any results if access to one of the media items is denied")]
+
---
+++ src/Sitecore.Support.117065/Search.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.ItemWebApi.Pipelines.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.ItemWebApi.Pipelines.Request
+{
+  public class Search : Sitecore.ItemWebApi.Pipelines.Request.Search
+  {
+    // Methods
+    public override void Process(RequestArgs args)
+    {
+      base.Process(args);
+      args.Items = (from x in args.Items
+                    where x != null
+                    select x).ToArray<Item>();
+    }
+  }
+
+
+}
